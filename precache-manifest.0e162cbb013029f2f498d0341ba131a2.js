220e3faab503144bf4b1dd85dfa420a915062b0b
Manel2313 <djemal.manel@gmail.com>
2019-05-10 10:34:02 +0100

    Updates

---
+++ precache-manifest.0e162cbb013029f2f498d0341ba131a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f7397cbd5838237c871c1cbc6dcf1e",
+    "url": "/docplanner-react/index.html"
+  },
+  {
+    "revision": "b4c648f9588e314905c0",
+    "url": "/docplanner-react/static/css/main.40f2a725.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/docplanner-react/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "b4c648f9588e314905c0",
+    "url": "/docplanner-react/static/js/main.e529faf4.chunk.js"
+  },
+  {
+    "revision": "3e50c450e9be5833b5b0",
+    "url": "/docplanner-react/static/js/runtime~main.5671041c.js"
+  }
+]);
